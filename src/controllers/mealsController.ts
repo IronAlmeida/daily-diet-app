f8a0b32e1ac92f6847449aaf0cbe95c6c50ee65e
Iron Almeida <ironsantos1999@gmail.com>
2023-10-09 04:03:25 -0300

    Create PUT and DELETE method in the meals route

--- src/controllers/mealsController.ts
+++ src/controllers/mealsController.ts
@@ -2,7 +2,7 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
 import { prisma } from "../prisma/prismaClient";
 
-export async function mealsController(
+export async function postMealsController(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
@@ -28,4 +28,63 @@ export async function mealsController(
   });
 
   reply.status(201).send("Refeição registrada!");
+}
+
+export async function putMealsController(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const putMealsSchema = z.object({
+    name: z
+      .string()
+      .min(3, "Obrigatório ao menos 3 caracteres para identificação"),
+    description: z
+      .string()
+      .min(3, "Obrigatório ao menos 3 caracteres para identificação"),
+    diet_on: z.boolean(),
+  });
+
+  const getReqParamsSchema = z.object({
+    id: z.string(),
+  });
+
+  const putMeals = putMealsSchema.parse(request.body);
+  const reqParams = getReqParamsSchema.parse(request.params);
+  const { id } = reqParams;
+
+  const { name, description, diet_on } = putMeals;
+
+  await prisma.meals.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      description,
+      diet_on,
+    },
+  });
+
+  reply.status(200).send({ message: "Refeição atualizada!" });
+}
+
+export async function deleteMealsController(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const getReqParamsSchema = z.object({
+    id: z.string(),
+  });
+
+  const reqParams = getReqParamsSchema.parse(request.params);
+
+  const { id } = reqParams;
+
+  await prisma.meals.delete({
+    where: {
+      id,
+    },
+  });
+
+  reply.status(204);
 }
--- src/routes/mealsRoute.ts
+++ src/routes/mealsRoute.ts
@@ -1,7 +1,19 @@
 import { FastifyInstance } from "fastify";
-import { mealsController } from "../controllers/mealsController";
+import {
+  postMealsController,
+  putMealsController,
+  deleteMealsController,
+} from "../controllers/mealsController";
 import { checkSectionTokenExists } from "../middlewares/check-section-token-exists";
 
 export async function mealsRoute(app: FastifyInstance) {
-  app.post("/", { onRequest: checkSectionTokenExists }, mealsController);
+  app.post("/", { onRequest: checkSectionTokenExists }, postMealsController);
+
+  app.put("/:id", { onRequest: checkSectionTokenExists }, putMealsController);
+
+  app.delete(
+    "/:id",
+    { onRequest: checkSectionTokenExists },
+    deleteMealsController
+  );
 }
